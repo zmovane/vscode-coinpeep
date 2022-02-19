38560f9c34cfb2b373380a13a533f0f14a04792c
Amovane <amovane@163.com>
2022-02-20 03:58:03 +0800

    Add provider/coinmarketcap

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,31 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { CoinmarketcapProvider } from "./provider/coinmarketcap";
 
+const extensionName: string = "coinpeep";
+const extensionID: string = "amovane.coinpeep";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "coinpeep" is now active!');
+  const apiKey: string | undefined = vscode.workspace
+    .getConfiguration("extensionName")
+    .get("coinmarketcap.apiKey");
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('coinpeep.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from coinpeep!');
-	});
+  const coinmarketcapProvider = new CoinmarketcapProvider(
+    extensionID,
+    extensionName,
+    apiKey
+  );
 
-	context.subscriptions.push(disposable);
+  vscode.window.registerTreeDataProvider(
+    "coinmarketcapTreeView",
+    coinmarketcapProvider
+  );
+
+  vscode.commands.registerCommand("coinmarketcapTreeView.refreshEntry", () =>
+    coinmarketcapProvider.refresh()
+  );
 }
 
 // this method is called when your extension is deactivated
---
+++ src/provider/coinmarketcap.ts
@@ -0,0 +1,133 @@
+import * as vscode from "vscode";
+import * as path from "path";
+import request from "../utils/http";
+import BigNumber from "bignumber.js";
+const ZERO = new BigNumber("0");
+export class CoinmarketcapProvider implements vscode.TreeDataProvider<Item> {
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    Item | undefined | null | void
+  > = new vscode.EventEmitter<Item | undefined | null | void>();
+  readonly onDidChangeTreeData: vscode.Event<Item | undefined | null | void> =
+    this._onDidChangeTreeData.event;
+
+  private apiKey: string | undefined;
+  private extensionName: string;
+  private stableCoin = "USDT";
+  private httpUrl = "https://pro-api.coinmarketcap.com/v1";
+
+  constructor(
+    extensionID: string,
+    extensionName: string,
+    apiKey: string | undefined
+  ) {
+    this.apiKey = apiKey;
+    this.extensionName = extensionName;
+
+    if (!this.apiKey) {
+      vscode.window
+        .showInformationMessage(
+          `${extensionName}: Please enter your CoinMarketCap API Key `,
+          "Add API Key"
+        )
+        .then((selection) => {
+          if (selection) {
+            vscode.commands.executeCommand(
+              "workbench.action.openSettings",
+              `@ext:${extensionID}`
+            );
+          }
+        });
+    }
+  }
+
+  getTreeItem(item: Item): vscode.TreeItem | Thenable<vscode.TreeItem> {
+    return item;
+  }
+  getChildren(): vscode.ProviderResult<Item[]> {
+    if (this.apiKey) {
+      return Promise.resolve(this._getItems());
+    } else {
+      return Promise.resolve([]);
+    }
+  }
+
+  refresh(): void {
+    this._onDidChangeTreeData.fire();
+  }
+
+  private async _getItems(): Promise<Item[]> {
+    const data = { start: 1, limit: 100, convert: this.stableCoin };
+    const header = {
+      "X-CMC_PRO_API_KEY": this.apiKey!,
+    };
+    const response = await request(
+      "GET",
+      `${this.httpUrl}/cryptocurrency/listings/latest`,
+      data,
+      header
+    );
+    const result: Response<Coin> = JSON.parse(response.data);
+    if (this._isOk(response.status, () => result.status.error_code === 0)) {
+      return result.data.map((coin: Coin) => this._fillItem(coin));
+    } else {
+      vscode.window.showErrorMessage(
+        `${this.extensionName}: Failed to request CoinMarketCap API`
+      );
+    }
+    return [];
+  }
+
+  private _fillItem(coin: Coin): Item {
+    const price = new BigNumber(coin.quote.USDT.price).precision(6);
+    const percent = new BigNumber(coin.quote.USDT.percent_change_24h);
+    const percentPrefix = percent.gte(ZERO) ? "+" : "";
+    const percentDisplay = `${percentPrefix}${percent.toFixed(2)}%`.padEnd(15);
+    const symbolDisplay = coin.symbol.padEnd(11);
+    const priceDisplay = `$${price}`;
+    const label =
+      symbolDisplay.padStart(symbolDisplay.length + 4) + percentDisplay;
+    const iconPath = percent.gt(ZERO)
+      ? this._iconPath("up.png")
+      : this._iconPath("down.png");
+    return new Item(label, iconPath, priceDisplay);
+  }
+
+  private _iconPath(iconName: string): string {
+    return path.join(__filename, "..", "..", "..", "resources", iconName);
+  }
+
+  private _isOk(status: number, otherCond: Function) {
+    return status >= 200 && status < 300 && otherCond();
+  }
+}
+interface Response<T> {
+  data: T[];
+  status: Status;
+}
+interface Status {
+  error_code: number;
+  error_message: string;
+}
+interface Coin {
+  name: string;
+  symbol: string;
+  quote: Quote;
+}
+interface Quote {
+  USDT: USDT;
+}
+interface USDT {
+  price: string;
+  volume_24h: string;
+  percent_change_24h: string;
+  percent_change_7d: string;
+  market_cap: string;
+}
+
+class Item extends vscode.TreeItem {
+  constructor(label: string, iconPath: vscode.Uri | string, price: string) {
+    super(label, vscode.TreeItemCollapsibleState.None);
+    this.iconPath = iconPath;
+    this.description = price;
+  }
+}
